9b3d3100477dd8a1b814645b7aa4acd3d2ad1b8d
mdataurrahman555 <74402921+mdataurrahman555@users.noreply.github.com>
2022-02-17 17:35:34 +0600

    Added comments on codes of all files

--- js/app.js
+++ js/app.js
@@ -1,33 +1,49 @@
+// Get All Input value With it function getInput()
+// Started
 function getInput(elem) {
     const getElement = document.getElementById(elem);
     const getElementValue = parseInt(getElement.value);
     return getElementValue;
 }
+// Ended
 
+// Get All tags with id it function name getInnertext()
+// Started
 function getInnerText(elem) {
     const getInnerText = document.getElementById(elem);
     return getInnerText;
 }
+// Ended
+
+// Get All Input Fields it function name setInputElement()
+// Started
 function setInputElement(elemInput) {
     const elemInputValue = getInput(elemInput + '-input');     
     return elemInputValue;
 }
+// ended
+
+// Get All Expenses it function name setExpensesElement()
+// Started
 function setExpensesElement(elemExpenses) {
     const elemExpensesValue = getInnerText(elemExpenses + '-expenses');
     return elemExpensesValue;
 }
-function setAmountBalance(elemTotalName) {
-    const elemExpensesValue = getInnerText(elemTotalName);
-    return elemExpensesValue;
-}
+// Ended
+
+// calculate-btn Start 
 
 document.getElementById('calculate-btn').addEventListener('click', function() {
+    // Decalare data and get data by funtions
+    
     const incomeValue = setInputElement('income');
     const foodValue = setInputElement('food');
     const rentValue = setInputElement('rent');
     const clothesValue = setInputElement('clothes');
     const totalExpenses = setExpensesElement('total');
     const balanceExpenses = setExpensesElement('balance');
+
+    // Here's the validations part Start
 
     if(
      (!isNaN(incomeValue) || (incomeValue === null) || (incomeValue === "")) &&
@@ -38,23 +54,30 @@ document.getElementById('calculate-btn').addEventListener('click', function() {
         totalExpenses.innerText = foodValue + rentValue + clothesValue;
         balanceExpenses.innerText = incomeValue - totalExpenses.innerText;
         if(parseInt(totalExpenses.innerText) > incomeValue) {
-            return alert('Sorry, Your Total Expenses Value larger than Income.Please Gives The Average Number in Your Income');
+            return alert('Sorry, Your Total Expenses Value larger than Income.Please Gives The Average Number in Your Income And Give The Positive Numeber');
         }
     }else {
         totalExpenses.innerText = 0;
         balanceExpenses.innerText = 0;
         return alert('Please Enter The Valid Data. Like a Number');
     }
+    // Here's the validations part End
 });
 
+// calculate-btn End
+
+// save-btn start
 
 document.getElementById('save-btn').addEventListener('click', function () {    
+    // Decalare data and get data by funtions
+
     const saveAmount = getInnerText('saving-amount');
     const incomeValue = setInputElement('income');
     const saveValue = setInputElement('save');
     const balanceExpenses = getInnerText('balance-expenses');
     const remainingBalance = getInnerText('remaining-balance');
 
+    // Here's the validations part Start
 
     if(
      (!isNaN(incomeValue) || (incomeValue === null) || (incomeValue === "")) && 
@@ -63,11 +86,13 @@ document.getElementById('save-btn').addEventListener('click', function () {
         saveAmount.innerText = (incomeValue * saveValue) / 100;
         remainingBalance.innerText = parseInt(balanceExpenses.innerText) - parseInt(saveAmount.innerText);
         if(parseInt(saveAmount.innerText) > parseInt(balanceExpenses.innerText)) {
-            return alert('Sorry, Your Remaing Value larger than Balance Expenses, Please Gives The Average Number in Your Balance Expenses');
+            return alert('Sorry, Your Remaing Value larger than Balance Expenses, Please Gives The Average Number in Your Balance Expenses And Give The Positive Numeber');
         }
     } else {
         saveAmount.innerText = 0;
         return alert('Please Enter The Valid Data. Like a Number');
     }   
+    // Here's the validations part End
 })
 
+// save-btn End
